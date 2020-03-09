0b1ca248877e19264d77dea2f506b2282e81763f
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2020-03-09 09:39:16 +0300

    Changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CodeforcesApiPy",
-    version="1.2.0",
+    version="1.2.1",
     description="Implementation of codeforces.com API",
     platforms="any",
     url="https://github.com/VadVergasov/CodeforcesApiPy",
