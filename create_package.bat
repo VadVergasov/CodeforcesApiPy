4ce26e7c2e6f50c9fd381fb3abd4642960c42b6e
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2020-06-15 23:24:27 +0300

    Added publish status to README

--- create_package.bat
+++
@@ -1,5 +0,0 @@
-RMDIR /Q /S build
-RMDIR /Q /S dist
-RMDIR /Q /S CodeforcesApiPy.egg-info
-python setup.py sdist bdist_wheel
-twine check dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CodeforcesApiPy",
-    version="1.2.4",
+    version="1.2.5",
     description="Implementation of codeforces.com API",
     platforms="any",
     url="https://github.com/VadVergasov/CodeforcesApiPy",
