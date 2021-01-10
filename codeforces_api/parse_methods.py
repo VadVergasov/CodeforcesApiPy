7895caefaadcb65ddf99ef2b149668d1a127680c
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2021-01-10 18:44:14 +0300

    Added more detailed logging

--- codeforces_api/parse_methods.py
+++ codeforces_api/parse_methods.py
@@ -9,6 +9,10 @@ class CodeforcesParser:
     def get_solution(self, contest_id, submit_id):
         """
         Returns source code for specified submit.
+
+        contest_id is the id of contest.
+
+        submit_id is the id of sumbission.
         """
         solutionPage = requests.get(
             "https://codeforces.com/contest/"
@@ -16,8 +20,10 @@ class CodeforcesParser:
             + "/submission/"
             + str(submit_id)
         )
+        if int(solutionPage.status_code) != 200:
+            raise Exception("Returned not OK code" + str(solutionPage))
         tree2 = html.fromstring(solutionPage.text)
         code = tree2.xpath('//*[@id="pageContent"]/div[3]/pre/text()')
         if len(code) == 0:
-            raise ValueError("Incorrect contest_id or submit_id")
+            raise ValueError("Incorrect contest_id or submit_id" + str(code))
         return code[0].replace("\r", "")
